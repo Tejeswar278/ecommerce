40c9d23c071176af57447765396fd85447e961b7
Muddada Tejeswara Rao <lsandptlove143@gmail.com>
2022-06-08 00:31:37 +0530

    getting user by id completed

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -18,7 +18,16 @@ router.post("/create", async (req,res) => {
         const user = await User.create(req.body);
         res.status(200).send(user)
     } catch (error) {
-        res.status(400).send({message:"something went wrong"})
+        res.status(400).send({message:error.message})
+    }
+})
+
+router.get("/:id", async (req,res) => {
+    try {
+        const user1 = await User.find({_id: {$eq: req.params.id}}).lean().exec(); 
+        res.status(200).send(user1)   
+    } catch (error) {
+        res.status(400).send({message:error.message})
     }
 })
 
