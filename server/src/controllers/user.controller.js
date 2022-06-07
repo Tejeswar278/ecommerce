19321b2de88668b9349488e403536a8811961c87
Muddada Tejeswara Rao <lsandptlove143@gmail.com>
2022-06-08 00:47:17 +0530

    completed crud for users

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -31,4 +31,40 @@ router.get("/:id", async (req,res) => {
     }
 })
 
+router.patch("/:id/edit", async (req,res) => {
+    try {
+        const user2 = await User.findByIdAndUpdate(req.params.id, req.body, {new :true}).lean().exec(); 
+        res.status(200).send(user2)   
+    } catch (error) {
+        res.status(400).send({message:error.message})
+    }
+})
+
+router.get("/:id/addressess", async (req,res) => {
+    try {
+        const user3 = await User.find({_id:{$eq:req.params.id}}).lean().exec(); 
+        res.status(200).send(user3.Address)   
+    } catch (error) {
+        res.status(400).send({message:error.message})
+    }
+})
+
+router.post("/:id/addressess/create", async (req,res) => {
+    try {
+        let user=await User.find({_id:{$eq:req.params.id}}).lean().exec()
+           user.Address.push(req.body)
+         let Updated=  await User.findByIdAndUpdate(req.params.id,user,{new:true}).lean().exec()
+    } catch (error) {
+        res.status(400).send({message:error.message})
+    }
+})
+
+router.post("/:id/addressess/idx/edit", async (req,res) => {
+    try {
+        let user=await User.find({_id:{$eq:req.params.id}}).lean().exec()
+    } catch (error) {
+        res.status(400).send({message:error.message})
+    }
+})
+
 module.exports = router;
